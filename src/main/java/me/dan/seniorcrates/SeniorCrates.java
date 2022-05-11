c6769b65aeef239030f71871600e2fe02ba60b9b
Dan-Java <owensdaniel48@gmail.com>
2022-05-11 22:53:49 +0100

    Initial commit

---
+++ src/main/java/me/dan/seniorcrates/SeniorCrates.java
@@ -0,0 +1,16 @@
+package me.dan.seniorcrates;
+
+import me.dan.pluginapi.plugin.CustomPlugin;
+
+public final class SeniorCrates extends CustomPlugin {
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void disable() {
+
+    }
+}
